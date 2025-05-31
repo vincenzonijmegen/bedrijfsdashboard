fdcb1efcd1eb09a671b66aa06d5691a4e648eb32
Herm van den Akker <herman@ijssalonvincenzo.nl>
2025-05-31 15:24:29 +0200

    disable eslint rule using flat config

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,17 @@
-/** @type {import('eslint').Linter.FlatConfig} */
-export default [
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
+import eslintPluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import * as prettier from "eslint-plugin-prettier";
+
+export default tseslint.config({
+  files: ["**/*.{ts,tsx}"],
+  extends: [
+    ...tseslint.configs.recommended,
+    eslintPluginJs.configs.recommended,
+    {
+      rules: {
+        "@typescript-eslint/no-explicit-any": "off",
+        "prettier/prettier": "off"
+      },
     },
-  },
-];
+  ],
+});
