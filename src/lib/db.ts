af7f164c4bf22c934395a3e5a9598df99e61fbc1
Herm van den Akker <herman@ijssalonvincenzo.nl>
2025-05-31 15:02:04 +0200

    fix: disable no-explicit-any for mock data

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -8,6 +8,7 @@ const pool = new Pool({
   },
 });
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const query = (text: string, params?: any[]) => {
-  return pool.query(text, params);
+return pool.query(text, params);
 };
