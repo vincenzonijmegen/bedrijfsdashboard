f8ee0e6c17757e7937f9039bf5dc7751573e7d31
Herm van den Akker <herman@ijssalonvincenzo.nl>
2025-06-06 10:58:48 +0200

    666

--- src/app/admin/instructies/[slug]/page.tsx
+++ src/app/admin/instructies/[slug]/page.tsx
@@ -1,10 +1,10 @@
 // src/app/admin/instructies/[slug]/page.tsx
 
-type Props = {
+export default async function InstructieBewerkPagina({
+  params,
+}: {
   params: { slug: string };
-};
-
-export default async function InstructieBewerkPagina({ params }: Props) {
+}) {
   const { slug } = params;
 
   return (
