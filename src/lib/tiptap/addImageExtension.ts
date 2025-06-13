ac169224f62a77802ec3230f7872a331285723e0
Herm van den Akker <herman@ijssalonvincenzo.nl>
2025-06-13 19:50:05 +0200

    Mails

--- src/lib/tiptap/addImageExtension.ts
+++ src/lib/tiptap/addImageExtension.ts
@@ -1,33 +1,48 @@
 import Image from "@tiptap/extension-image";
+import type { CommandProps } from "@tiptap/core";
 
 export function addImageExtension(onUpload: (file: File) => Promise<string>) {
   return Image.extend({
+    addOptions() {
+      return {
+        ...this.parent?.(),
+        allowBase64: true,
+        inline: false,
+      };
+    },
+
     addCommands() {
       return {
         ...this.parent?.(),
         setImageFromUpload:
           () =>
-          async ({ chain }) => {
-            const input = document.createElement("input");
-            input.type = "file";
-            input.accept = "image/*";
-            input.click();
-            return new Promise((resolve) => {
-              input.onchange = async () => {
-                const file = input.files?.[0];
-                if (file) {
-                  const url = await onUpload(file);
-                  chain().focus().setImage({ src: url }).run();
-                }
-                resolve(true);
-              };
-            });
-          },
+            async (props: CommandProps) => {
+              const { chain } = props;
+
+              const input = document.createElement("input");
+              input.type = "file";
+              input.accept = "image/*";
+              input.click();
+
+              return new Promise((resolve) => {
+                input.onchange = async () => {
+                  const file = input.files?.[0];
+                  if (file) {
+                    const url = await onUpload(file);
+                    chain().focus().setImage({ src: url }).run();
+                  }
+                  resolve(true);
+                };
+              });
+            },
       };
     },
+
     addKeyboardShortcuts() {
       return {
-        "Mod-Shift-I": () => this.editor.commands.setImageFromUpload(),
+        ...this.parent?.(),
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        "Mod-Shift-I": () => (this.editor.commands as any).setImageFromUpload(),
       };
     },
   });
