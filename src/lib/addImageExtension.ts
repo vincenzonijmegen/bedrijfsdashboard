847f7436a09b0eb72913841525fd21eb7da32ef3
Herm van den Akker <herman@ijssalonvincenzo.nl>
2025-06-13 19:05:46 +0200

    Mails

--- src/lib/addImageExtension.ts
+++ src/lib/addImageExtension.ts
@@ -39,9 +39,9 @@ export function addImageExtension(onUpload: (file: File) => Promise<string>) {
     addKeyboardShortcuts() {
       return {
         ...this.parent?.(),
-       // @ts-expect-error: custom command via Image extension
+        // @ts-expect-error - custom command not in Tiptap types
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         "Mod-Shift-I": () => (this.editor.commands as any).setImageFromUpload(),
-
       };
     },
   });
