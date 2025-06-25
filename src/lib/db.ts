360519b95afec73d6101a97d56caf782c1d777b5
Herm van den Akker <herman@ijssalonvincenzo.nl>
2025-06-25 22:41:40 +0200

    skills 372

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,15 +1,9 @@
-// lib/db.ts
 import { Pool } from "pg";
-
-import postgres from "postgres";
-export const sql = postgres(process.env.DATABASE_URL!);
-
-
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
 });
 
 export const db = {
-query: (text: string, params: unknown[] = []) => pool.query(text, params),
+  query: (text: string, params: unknown[] = []) => pool.query(text, params),
 };
