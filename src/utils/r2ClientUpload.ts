3a7dc08ae45334441ed9d0a77c8d2cededbb7a30
Herm van den Akker <herman@ijssalonvincenzo.nl>
2025-06-28 11:50:18 +0200

    Fix: importeer r2ClientUpload als uploadAfbeelding

--- src/utils/r2ClientUpload.ts
+++ src/utils/r2ClientUpload.ts
@@ -1,6 +1,6 @@
-export function r2ClientUpload(file: File): Promise<string> {
+export function uploadAfbeelding(file: File): Promise<string> {
   return Promise.resolve("https://dummy.com/" + file.name);
 }
 
 // Gebruik in andere bestanden:
-// import { r2ClientUpload as uploadAfbeelding } from "@/utils/r2ClientUpload";
+// import { uploadAfbeelding } from "@/utils/r2ClientUpload";
