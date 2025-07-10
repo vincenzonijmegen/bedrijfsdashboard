0d4aa3219a1dac3a8c23d990fa03cb2cf4cba475
Herm van den Akker <herman@ijssalonvincenzo.nl>
2025-07-10 19:18:11 +0200

    Actielijst

--- src/app/admin/acties/page.tsx
+++ src/app/admin/acties/page.tsx
@@ -181,7 +181,7 @@ export default function ActieLijstPagina() {
               value={nieuweActieTekst}
               onChange={(e) => setNieuweActieTekst(e.target.value)}
             />
-            <button onClick={nieuweActieToevoegen} className="bg-blue-500 text-white px-3 rounded">
+            <button onClick={nieuweActieToevoegen} className="bg-blue-500 text-yellow-300 px-3 rounded text-xl font-bold">
               ➕
             </button>
           </div>
