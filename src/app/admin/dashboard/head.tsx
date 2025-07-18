39bb2aaf2fe2d5f573e996413a1f4a3bd300a5d2
Herm van den Akker <herman@ijssalonvincenzo.nl>
2025-07-18 19:52:49 +0200

    Omzet live data

--- src/app/admin/dashboard/head.tsx
+++ src/app/admin/dashboard/head.tsx
@@ -1,18 +1,17 @@
 // src/app/admin/dashboard/head.tsx
 import React from 'react';
 
+// src/app/head.tsx
 export default function Head() {
   return (
     <>
-      <title>Dashboard – Vincenzo</title>
-      {/* Specifiek iPhone‐icoon voor deze subdir */}
+      {/* … je bestaande tags … */}
       <link
         rel="apple-touch-icon"
         sizes="180x180"
         href="/icons/apple-touch-icon.png"
       />
-      {/* Optioneel eigen favicon voor deze subdir */}
-      <link rel="icon" href="/icons/dashboard-favicon.ico" />
     </>
-  );
+  )
 }
+
